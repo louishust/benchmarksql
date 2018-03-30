cbb3236ac64ea92f2342e5f64b0d28c4af4310c5
syd <sydnever@foxmail.com>
2018-03-30 10:19:19 +0800

    first version

--- src/client/jTPCC.java
+++ src/client/jTPCC.java
@@ -119,7 +119,9 @@ public class jTPCC implements jTPCCConfig
 	else if (iDB.equals("oracle"))
 	    dbType = DB_ORACLE;
 	else if (iDB.equals("postgres"))
-	    dbType = DB_POSTGRES;
+		dbType = DB_POSTGRES;
+	else if (iDB.equals("mysql"))
+		dbType = DB_MYSQL;
 	else
 	{
 	    log.error("unknown database type '" + iDB + "'");
